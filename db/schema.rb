ea5cb70ad7c8d38f1a773dac69442055fe815faa
Camille Baillat <camille.baillat@protonmail.com>
2018-06-28 11:47:20 +0200

    [model] Added a unique index and a slug field in User model for friendly ID

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_27_155344) do
+ActiveRecord::Schema.define(version: 2018_06_28_093739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,9 +46,11 @@ ActiveRecord::Schema.define(version: 2018_06_27_155344) do
     t.string "firstname"
     t.string "surname"
     t.datetime "deleted_at"
+    t.string "slug", null: false
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
 end
